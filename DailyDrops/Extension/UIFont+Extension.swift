562cdf44f5391640ffcfa293586a01ed673ab87c
chasomin <somin49@naver.com>
2024-03-07 17:33:54 +0900

    Feat: UIFont Extension Custom Font 설정

---
+++ DailyDrops/Extension/UIFont+Extension.swift
@@ -0,0 +1,23 @@
+//
+//  UIFont+Extension.swift
+//  DailyDrops
+//
+//  Created by 차소민 on 3/7/24.
+//
+
+import UIKit
+
+extension UIFont {
+    static let largeBoldTitle = UIFont(name: "Pretendard-Bold", size: 34)
+    
+    static let boldTitle = UIFont(name: "Pretendard-Bold", size: 20)
+    static let title = UIFont(name: "Pretendard-Regular", size: 20)
+    
+    static let boldBody = UIFont(name: "Pretendard-Bold", size: 16)
+    static let body = UIFont(name: "Pretendard-Regular", size: 16)
+    
+    static let boldCallout = UIFont(name: "Pretendard-Bold", size: 13)
+    static let callout = UIFont(name: "Pretendard-Regular", size: 13)
+    
+    static let caption = UIFont(name: "Pretendard-Regular", size: 11)
+}
