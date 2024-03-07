c2a8b0db25dd0c539ae42c6815ac4d89921a1e6e
chasomin <somin49@naver.com>
2024-03-07 17:08:13 +0900

    Feat: UIColor Extension Custom Color 설정

---
+++ DailyDrops/Extension/UIColor+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  UIColor+Extension.swift
+//  DailyDrops
+//
+//  Created by 차소민 on 3/7/24.
+//
+
+import UIKit
+
+extension UIColor {
+    static let pointColor = UIColor.systemTeal
+    static let secondaryColor = UIColor(red: 252/255, green: 249/255, blue: 187/255, alpha: 1)
+    static let backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+    static let titleColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+    static let subTitleColor = UIColor.gray
+}
