7fa7311270a94e3bd005654c6f9b542333039a2d
chasomin <somin49@naver.com>
2024-03-09 02:04:01 +0900

    Fix: Supplement Model 필요 없는 코드 제거

--- DailyDrops/Model/Supplement.swift
+++ DailyDrops/Model/Supplement.swift
@@ -11,12 +11,6 @@ struct Info {
     let totalCount: String
     let dataList: [Supplement]      // data 없으면 오류처리
     let result: Result
-    
-    enum CodingKeys: String, CodingKey {
-        case totalCount = "total_count"
-        case dataList = "row"
-        case result = "RESULT"
-    }
 }
 
 struct Supplement {
@@ -24,21 +18,9 @@ struct Supplement {
     let shape: String                    // 영양제 형태 (ex.캡슐 / 정)
     let corporateName: String            // 회사명
     let intakeMethod: String             // 섭취 방법
-    
-    enum CodingKeys: String, CodingKey {
-        case supplementName = "PRDLST_NM"
-        case shape = "PRDT_SHAP_CD_NM"
-        case corporateName = "BSSH_NM"
-        case intakeMethod = "NTK_MTHD"
-    }
 }
 
 struct Result {
     let message: String
     let code: String
-    
-    enum CodingKeys: String, CodingKey {
-        case message = "MSG"
-        case code = "CODE"
-    }
 }
