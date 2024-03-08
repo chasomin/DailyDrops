7ac0769c2fa3b3d56b45190ec3bb45ac7443c130
chasomin <somin49@naver.com>
2024-03-09 02:44:43 +0900

    Feat: WaterViewController 프로퍼티 접근 제어자 적용

--- DailyDrops/Water/WaterViewController.swift
+++ DailyDrops/Water/WaterViewController.swift
@@ -9,20 +9,24 @@ import UIKit
 import WaveAnimationView
 import SnapKit
 
+//TODO: Realm
 final class WaterViewController: BaseViewController {
-    let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
-    lazy var window = windowScene?.windows.first
-    lazy var safeTop = window?.safeAreaInsets.top ?? 0 + (self.navigationController?.navigationBar.frame.height ?? 0)   // safeArea + Navigation
-    lazy var safeBottom = window?.safeAreaInsets.bottom ?? 0
+    private let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene
+    private lazy var window = windowScene?.windows.first
+    private lazy var safeTop = window?.safeAreaInsets.top ?? 0 + (self.navigationController?.navigationBar.frame.height ?? 0)   // safeArea + Navigation
+    private lazy var safeBottom = window?.safeAreaInsets.bottom ?? 0
     
     
-    lazy var waveView = WaveAnimationView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height - safeTop ), frontColor: .white, backColor: .systemTeal)
-    let countLabel = UILabel()
-    let goalTest: Float = 20
-    let currentTest: Float = 1
+    private lazy var waveView = WaveAnimationView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height - safeTop ), frontColor: .white, backColor: .systemTeal)
+    private let countLabel = UILabel()
+    private let goalTest: Float = 20
+    private var currentTest: Float = 1
+    //setNeedsDisplay()?
         
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        waveView.startAnimation()
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.title = "목표! \(Int(goalTest))잔 마시기"
     }
