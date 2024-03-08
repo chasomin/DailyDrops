99ea340aafe73dd66ec71b0635063834fe168ab6
chasomin <somin49@naver.com>
2024-03-09 02:41:25 +0900

    Feat: SupplementViewContoller 생성

---
+++ DailyDrops/Supplement/SupplementViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SupplementViewController.swift
+//  DailyDrops
+//
+//  Created by 차소민 on 3/9/24.
+//
+
+import UIKit
+
+class SupplementViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationItem.title = "내 영양제 관리"
+    }
+    
+    override func configureHierarchy() {
+        
+    }
+    
+    override func configureLayout() {
+        
+    }
+    
+    override func configureView() {
+        
+    }
+}
