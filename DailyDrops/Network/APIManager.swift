3a7bae77dfe79af47d4bcb2763b5dff895d8ee43
chasomin <somin49@naver.com>
2024-03-10 23:54:53 +0900

    Fix: Request 응답 ServiceDTO struct로 변경

--- DailyDrops/Network/APIManager.swift
+++ DailyDrops/Network/APIManager.swift
@@ -14,10 +14,11 @@ final class APIManager {
     
     //TODO: ERROR 핸들링, Toast
     func callRequest(api: SupplementAPI, completionHandler: @escaping ([Supplement]?, Error?) -> Void) {
-        AF.request(api.endPoint).responseDecodable(of: InfoDTO.self) { response in
+        AF.request(api.endPoint).responseDecodable(of: ServiceDTO.self) { response in
             switch response.result {
             case .success(let success):
-                completionHandler(success.toEntity().dataList, nil)
+                completionHandler(success.toEntity().serviceID.dataList, nil)
+                print(success)
             case .failure(let failure):
                 completionHandler(nil, failure)
             }
