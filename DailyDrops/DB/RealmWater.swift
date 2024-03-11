bb6486f0eb2ed2239ee99c8f69f7046c64b71b37
chasomin <somin49@naver.com>
2024-03-11 20:41:40 +0900

    Fix: Realm Water 모델 수정 (Int에서 Float으로 변경, 외래키 삭제)

--- DailyDrops/DB/RealmWater.swift
+++ DailyDrops/DB/RealmWater.swift
@@ -9,28 +9,24 @@ import Foundation
 import RealmSwift
 
 final class RealmWater: Object {
-    let repository = RealmRepository()
     
     @Persisted(primaryKey: true) var id: ObjectId
     @Persisted var date: Date
-    @Persisted var drinkWater: Int
-    @Persisted var goal: ObjectId
+    @Persisted var drinkWater: Float
     
-    convenience init(drinkWater: Int, goal: ObjectId) {
+    convenience init(drinkWater: Float, goal: ObjectId) {
         self.init()
         self.date = Date()
         self.drinkWater = drinkWater
-        self.goal = goal
     }
     
     func toEntity() -> Water {
-        return Water(id: id.stringValue, date: date, drinkWater: drinkWater, goal: repository.readGoalCups(id: goal))
+        return Water(id: id.stringValue, date: date, drinkWater: drinkWater)
     }
 }
 
 struct Water {
     let id: String
     let date: Date
-    let drinkWater: Int
-    let goal: Int
+    let drinkWater: Float
 }
