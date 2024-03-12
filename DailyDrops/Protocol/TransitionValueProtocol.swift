6b59f45dd82e10c5ed2ed8d680d489f4c5860ccd
chasomin <somin49@naver.com>
2024-03-12 18:49:52 +0900

    Feat: 데이터 전달하는 Protocol 생성

---
+++ DailyDrops/Protocol/TransitionValueProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  TransitionValueProtocol.swift
+//  DailyDrops
+//
+//  Created by 차소민 on 3/12/24.
+//
+
+import Foundation
+
+protocol TransitionValue {
+    func transition(value: String)
+}
