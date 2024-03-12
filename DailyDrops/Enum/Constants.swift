e7a135c282119ce3d55b005b320d17b3cf62b9ff
chasomin <somin49@naver.com>
2024-03-12 20:54:21 +0900

    Feat: WeekButtonTitle Enum 생성

---
+++ DailyDrops/Enum/Constants.swift
@@ -0,0 +1,24 @@
+//
+//  Constants.swift
+//  DailyDrops
+//
+//  Created by 차소민 on 3/12/24.
+//
+
+import Foundation
+
+enum Constants {
+    enum NavigationTitle {
+        
+    }
+    
+    enum WeekButtonTitle: String, CaseIterable {
+        case mon = "월"
+        case tue = "화"
+        case wed = "수"
+        case thu = "목"
+        case fri = "금"
+        case sat = "토"
+        case sun = "일"
+    }
+}
