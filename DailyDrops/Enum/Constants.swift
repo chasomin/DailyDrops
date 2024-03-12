54d1ef1344603ea4376c180dfe8c0fee5335ba7f
chasomin <somin49@naver.com>
2024-03-12 21:28:28 +0900

    Feat: 각 화면마다 사용될 NavigationTitle enum 생성

--- DailyDrops/Enum/Constants.swift
+++ DailyDrops/Enum/Constants.swift
@@ -8,7 +8,26 @@
 import Foundation
 
 enum Constants {
-    
+    enum NavigationTitle {
+        case Water(goal: Int)
+        case MySupplement
+        case SetNotification
+        case SearchSupplement
+        
+        var title: String {
+            switch self {
+            case .Water(let goal):
+                return "목표! \(goal)잔 마시기"
+            case .MySupplement:
+                return "내 영양제 관리"
+            case .SetNotification:
+                return "알림 설정"
+            case .SearchSupplement:
+                return "영양제 검색"
+            }
+        }
+    }
+
     enum WeekButtonTitle: String, CaseIterable {
         case mon = "월"
         case tue = "화"
