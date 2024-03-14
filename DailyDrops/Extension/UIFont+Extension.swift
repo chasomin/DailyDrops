7bd2beb19f41a77df84059af308c9fca8ee6caeb
chasomin <somin49@naver.com>
2024-03-14 16:11:56 +0900

    Fix: UIFont 옵셔널 처리

--- DailyDrops/Extension/UIFont+Extension.swift
+++ DailyDrops/Extension/UIFont+Extension.swift
@@ -8,16 +8,16 @@
 import UIKit
 
 extension UIFont {
-    static let largeBoldTitle = UIFont(name: "Pretendard-Bold", size: 34)
+    static let largeBoldTitle = UIFont(name: "Pretendard-Bold", size: 34) ?? UIFont()
     
-    static let boldTitle = UIFont(name: "Pretendard-Bold", size: 20)
-    static let title = UIFont(name: "Pretendard-Regular", size: 20)
+    static let boldTitle = UIFont(name: "Pretendard-Bold", size: 20) ?? UIFont()
+    static let title = UIFont(name: "Pretendard-Regular", size: 20) ?? UIFont()
     
-    static let boldBody = UIFont(name: "Pretendard-Bold", size: 16)
-    static let body = UIFont(name: "Pretendard-Regular", size: 16)
+    static let boldBody = UIFont(name: "Pretendard-Bold", size: 16) ?? UIFont()
+    static let body = UIFont(name: "Pretendard-Regular", size: 16) ?? UIFont()
     
-    static let boldCallout = UIFont(name: "Pretendard-Bold", size: 13)
-    static let callout = UIFont(name: "Pretendard-Regular", size: 13)
+    static let boldCallout = UIFont(name: "Pretendard-Bold", size: 13) ?? UIFont()
+    static let callout = UIFont(name: "Pretendard-Regular", size: 13) ?? UIFont()
     
-    static let caption = UIFont(name: "Pretendard-Regular", size: 11)
+    static let caption = UIFont(name: "Pretendard-Regular", size: 11) ?? UIFont()
 }
