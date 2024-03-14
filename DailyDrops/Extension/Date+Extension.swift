3ece6dd5165eca681b99ca389162a3b5e06fc84f
chasomin <somin49@naver.com>
2024-03-15 01:29:17 +0900

    Feat: DateFormatter로 Date 요일을 숫지로 리턴하는 메서드 구현

--- DailyDrops/Extension/Date+Extension.swift
+++ DailyDrops/Extension/Date+Extension.swift
@@ -14,15 +14,16 @@ extension Date {
         return format.string(from: self)
     }
     
-//    func dateFilterDay() -> String {
-//        let format = DateFormatter()
-//        format.dateFormat = "E"
-//        return format.string(from: self)
-//    }
+    func dateFilterDay() -> Int {
+        let format = DateFormatter()
+        format.dateFormat = "e" // 일~토 =>  1~7
+        guard let result = Int(format.string(from: self)) else { return 0}
+        return result
+    }
     
     func dateFilterTime() -> String {
         let format = DateFormatter()
-        format.dateFormat = "H:m"
+        format.dateFormat = "HH:mm"
         return format.string(from: self)
     }
 }
