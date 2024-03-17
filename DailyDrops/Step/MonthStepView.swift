2a99db5e4df80cec914c338154956ed4d4f0221d
chasomin <somin49@naver.com>
2024-03-17 23:27:13 +0900

    Feat: 한 달 간의 걸음 수를 볼 수 있는 Chart뷰 구현

---
+++ DailyDrops/Step/MonthStepView.swift
@@ -0,0 +1,68 @@
+//
+//  MonthStepView.swift
+//  DailyDrops
+//
+//  Created by 차소민 on 3/17/24.
+//
+
+import Foundation
+import SnapKit
+import DGCharts
+
+final class MonthStepView: BaseView {
+    let chartView = BarChartView()
+    var steps: [Double]
+    
+    init(steps: [Double], frame: CGRect) {
+        self.steps = steps
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func configureHierarchy() {
+        addSubview(chartView)
+
+    }
+    
+    override func configureLayout() {
+        chartView.snp.makeConstraints { make in
+            make.edges.equalTo(safeAreaLayoutGuide)
+        }
+    }
+    
+    override func configureView() {
+        setDataCount(steps.count, range: UInt32(10000))
+    }
+
+    func setDataCount(_ count: Int, range: UInt32) {
+        let yVals = (0..<count).map { (i) -> BarChartDataEntry in
+            let data = steps
+            return BarChartDataEntry(x: Double(i), y: Double(data[i]))
+        }
+        
+        var set1: BarChartDataSet! = nil
+        if let set = chartView.data?.first as? BarChartDataSet {
+            set1 = set
+            set1.replaceEntries(yVals)
+            chartView.data?.notifyDataChanged()
+            chartView.notifyDataSetChanged()
+        } else {
+            set1 = BarChartDataSet(entries: yVals)
+            set1.colors = [.systemTeal]
+            set1.drawValuesEnabled = false
+            
+            let data = BarChartData(dataSet: set1)
+            data.barWidth = 0.7
+            
+            chartView.data = data
+            chartView.doubleTapToZoomEnabled = false
+            chartView.xAxis.enabled = false
+            chartView.leftAxis.enabled = false
+            chartView.rightAxis.enabled = false
+            chartView.legend.enabled = false
+        }
+    }
+}
