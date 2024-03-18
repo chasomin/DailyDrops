0566acd8688e9af360c79af0c1ea9d5ee53f7fd3
chasomin <somin49@naver.com>
2024-03-18 18:21:11 +0900

    Feat: 영양제 복용 버튼 클릭 시 log테이블에 해당 데이터 존재 유무 확인 후 있으면 삭제, 없으면 추가

--- DailyDrops/Supplement/MySupplement/MySupplementViewModel.swift
+++ DailyDrops/Supplement/MySupplement/MySupplementViewModel.swift
@@ -12,12 +12,13 @@ final class MySupplementViewModel {
     
     let inputViewDidLoad: Observable<Void?> = Observable(nil)
     let inputViewWillAppear: Observable<Void?> = Observable(nil)
+    let inputCheckButtonTapped: Observable<(section: String, supplement: String)?> = Observable(nil)
     
     let outputViewDidLoad: Observable<Void?> = Observable(nil)
     let outputSetNavigation: Observable<Void?> = Observable(nil)
     let outputSection: Observable<[String]> = Observable([])
     let outputSupplementData : Observable<[String:[SupplementName]]?> = Observable(nil)
-    
+    let outputCheckButtonTapped: Observable<Void?> = Observable(nil)
     
     init() {
         transform()
@@ -35,6 +36,17 @@ final class MySupplementViewModel {
             guard let value else { return }
             outputSupplementData.value = [:]
             setTodaySupplementForTime()
+        }
+        inputCheckButtonTapped.bind { [weak self] value in
+            guard let self, let value else { return }
+            
+            if repository.readSupplementLog().filter({ $0.supplementName == value.supplement && $0.supplementTime == value.section && $0.regDate.dateFormat() == Date().dateFormat() }).isEmpty {
+                repository.createItem(RealmSupplementLog(supplementName: value.supplement, supplementTime: value.section), completion: nil)
+                outputCheckButtonTapped.value = ()
+            } else {
+                repository.deleteSupplementLog(date: Date().dateFormat(), name: value.supplement, time: value.section)
+                outputCheckButtonTapped.value = ()
+            }
         }
     }
     
