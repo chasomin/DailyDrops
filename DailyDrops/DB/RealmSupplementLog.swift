b7fa49029aa80bb845abeb75a74ebcbd0c039d84
chasomin <somin49@naver.com>
2024-03-18 18:19:50 +0900

    Feat: Realm 영양제 로그 model 구현

--- DailyDrops/DB/RealmSupplementLog.swift
+++ DailyDrops/DB/RealmSupplementLog.swift
@@ -6,3 +6,32 @@
 //
 
 import Foundation
+import RealmSwift
+
+final class RealmSupplementLog: Object {
+    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted var regDate: Date
+    @Persisted var supplementName: String
+    @Persisted var supplementTime: String   // 영양제 하나가 하루에 3번까지 알림 가능해서 영양제 이름+시간으로 특정 영양제 섭취 유무 확인 가능
+
+    convenience init(supplementName: String, supplementTime: String) {
+        self.init()
+        self.regDate = Date()
+        self.supplementName = supplementName
+        self.supplementTime = supplementTime
+    }
+    
+    func toEntity() -> SupplementLog {
+        return SupplementLog(regDate: regDate, name: supplementName, time: supplementTime)
+    }
+}
+
+struct SupplementLog {
+    let regDate: Date
+    let name: String
+    let time: String
+    
+    func toDTO() -> RealmSupplementLog {
+        return RealmSupplementLog(supplementName: name, supplementTime: time)
+    }
+}
