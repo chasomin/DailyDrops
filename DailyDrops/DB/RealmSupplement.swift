c886d11cc79287747631be6b16e2a8cf357664a0
chasomin <somin49@naver.com>
2024-03-21 01:07:56 +0900

    Fix: id 타입을 UUID로 변경

--- DailyDrops/DB/RealmSupplement.swift
+++ DailyDrops/DB/RealmSupplement.swift
@@ -9,7 +9,8 @@ import Foundation
 import RealmSwift
 
 final class RealmSupplement: Object {
-    @Persisted(primaryKey: true) var id: ObjectId
+    @Persisted(primaryKey: true) var id: UUID
+    @Persisted var regDate: Date
     @Persisted var name: String
     @Persisted var days: List<Int>
     @Persisted var times: List<Date>
@@ -17,12 +18,13 @@ final class RealmSupplement: Object {
     convenience init(name: String, days: List<Int>, times: List<Date>) {
         self.init()
         
+        self.regDate = Date()
         self.name = name
         self.days = days
         self.times = times
     }
     
     func toEntity() -> MySupplement {
-        return MySupplement(name: name, days: days.map{$0}, times: times.map{$0})
+        return MySupplement(id: id, regDate: regDate, name: name, days: days.map{$0}, times: times.map{$0})
     }
 }
