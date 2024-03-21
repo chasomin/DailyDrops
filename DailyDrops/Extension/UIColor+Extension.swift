5338a448c7b46e79048630177bbad5f81f518560
chasomin <somin49@naver.com>
2024-03-22 04:10:10 +0900

    Feat: background 컬러 추가, Bool 값에 따라 리턴 컬러 정해지는 메서드 구현

--- DailyDrops/Extension/UIColor+Extension.swift
+++ DailyDrops/Extension/UIColor+Extension.swift
@@ -11,6 +11,18 @@ extension UIColor {
     static let pointColor = UIColor.systemTeal
     static let secondaryColor = UIColor(red: 252/255, green: 249/255, blue: 187/255, alpha: 1)
     static let backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
+    static let secondaryBackgroundColor = UIColor(red: 236/255, green: 236/255, blue: 236/255, alpha: 1)
     static let titleColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
     static let subTitleColor = UIColor.gray
+}
+
+extension UIColor {
+    static func setStatusColor(status: Bool) -> UIColor {
+        switch status {
+        case true:
+            return UIColor.secondaryColor
+        case false:
+            return UIColor.pointColor
+        }
+    }
 }
