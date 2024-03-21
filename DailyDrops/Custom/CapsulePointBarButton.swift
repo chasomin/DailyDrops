6739760c5fee1bd53ff6047c307e9c8b7a500ff9
chasomin <somin49@naver.com>
2024-03-22 04:11:41 +0900

    Feat: Shadow 추가 구현

--- DailyDrops/Custom/CapsulePointBarButton.swift
+++ DailyDrops/Custom/CapsulePointBarButton.swift
@@ -22,6 +22,11 @@ final class CapsulePointButton: UIButton {
         attr.font = UIFont.body
         config.attributedSubtitle = attr
         self.configuration = config
+        self.layer.shadowColor = UIColor.subTitleColor.cgColor
+        self.layer.shadowOpacity = 0.5
+        self.layer.shadowRadius = 2
+        self.layer.shadowOffset = CGSize(width: 0, height: 0)
+
     }
     
     required init?(coder: NSCoder) {
