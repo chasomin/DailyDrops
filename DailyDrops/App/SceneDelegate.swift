661a4b374adb7cd4fd6041929ca2a6f15250c59a
chasomin <somin49@naver.com>
2024-03-23 18:39:41 +0900

    Feat: 앱 진입 시 전달 되어있던 Notification과 뱃지 제거

--- DailyDrops/App/SceneDelegate.swift
+++ DailyDrops/App/SceneDelegate.swift
@@ -34,8 +34,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+        // 뱃지 제거
+        UIApplication.shared.applicationIconBadgeNumber = 0
+        
+        // 사용자에게 이미 전달되어 있는 노티들을 제거 (ex. 카카오톡)
+        UNUserNotificationCenter.current().removeAllDeliveredNotifications()
     }
 
     func sceneWillResignActive(_ scene: UIScene) {
