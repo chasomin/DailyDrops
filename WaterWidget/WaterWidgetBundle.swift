3514d797a02d7f0a924d9a1e40da1f81129871f0
chasomin <somin49@naver.com>
2024-05-13 22:34:22 +0900

    Chore: 위젯 추가

---
+++ WaterWidget/WaterWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WaterWidgetBundle.swift
+//  WaterWidget
+//
+//  Created by 차소민 on 5/13/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WaterWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        WaterWidget()
+    }
+}
