81078c76cca93c3b72f952b069fb4a6d38837678
chasomin <somin49@naver.com>
2024-05-17 22:42:42 +0900

    Design: PointColor RGB값으로 변경

--- DailyDrops/Extension/UIColor+Extension.swift
+++ DailyDrops/Extension/UIColor+Extension.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 extension UIColor {
-    static let pointColor = UIColor.systemTeal
+    static let pointColor = UIColor(red: 48/255, green: 176/255, blue: 199/255, alpha: 1)
     static let secondaryColor = UIColor(red: 252/255, green: 249/255, blue: 187/255, alpha: 1)
     static let backgroundColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
     static let secondaryBackgroundColor = UIColor(red: 236/255, green: 236/255, blue: 236/255, alpha: 1)
